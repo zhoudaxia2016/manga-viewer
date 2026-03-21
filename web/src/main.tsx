16be98d6e5101753dcd3bba7a31c32de7f9f75af
maozhongzhou <maozhongzhou@kingsoft.com>
2026-03-21 22:58:50 +0800

    feat: 部署打开白页

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+const base = import.meta.env.BASE_URL;
+const routerBasename =
+  base === '/' ? undefined : base.replace(/\/$/, '');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
