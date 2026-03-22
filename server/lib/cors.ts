738659131a693ed07b5bdfccd89a87721efcfdb3
maozhongzhou <maozhongzhou@kingsoft.com>
2026-03-22 16:32:04 +0800

    fix: 部署环境删除漫画失败

--- server/lib/cors.ts
+++ server/lib/cors.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 };
 
